f1660aaf71fca65a073e89d37fc9e1efaa9fd2bf
kiqrr <caiquerr99@gmail.com>
2025-08-04 19:17:01 -0300

    First Commit

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import MeuComponente from './components/MeuComponente';
+import Contador from './components/Contador';
+
+function App() {
+  return (
+    <div className="App">
+      <MeuComponente />
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
+
